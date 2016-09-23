047dfd9c45e2d3d3bacf07254f2dec3b2dafa3a6
Laurent Prevost <laurent.prevost@probedock.io>
2016-09-23 14:59:56 +0200

    Fixed script issue

--- images/sensu-server/install-plugins.sh
+++ images/sensu-server/install-plugins.sh
@@ -3,3 +3,8 @@
 for plugin in `cat /plugins.txt`; do
   sensu-install -p $plugin
 done
+
+
+for gem in /gems/*.gem; do
+  GEM_PATH=/opt/sensu/embedded/lib/ruby/gems/2.3.0 /opt/sensu/embedded/bin/gem install $gem
+done
--- install-plugins.sh
+++ install-plugins.sh
@@ -4,6 +4,6 @@ for plugin in `cat images/sensu-server/plugins.txt`; do
   sudo sensu-install -p $plugin
 done
 
-for gem in images/sensu-server/gems; do
-  sudo GEM_PATH=/opt/sensu/embedded/lib/ruby/gems/2.3.0 /opt/sensu/embedded/bin/gem install images/sensu-server/gems/$gem
+for gem in images/sensu-server/gems/*.gem; do
+  sudo GEM_PATH=/opt/sensu/embedded/lib/ruby/gems/2.3.0 /opt/sensu/embedded/bin/gem install $gem
 done
