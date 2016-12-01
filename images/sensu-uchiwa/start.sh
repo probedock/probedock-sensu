cada118d7d5daabde6671b1d8fd9e8da72391b95
Laurent Prevost <laurent.prevost@probedock.io>
2016-12-01 11:08:33 +0100

    Experiments

--- images/sensu-uchiwa/start.sh
+++ images/sensu-uchiwa/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-until (echo > "/dev/tcp/sensu-redis/6379") >/dev/null 2>&1; do
+until (echo > "/dev/tcp/sensu-api/4567") >/dev/null 2>&1; do
   >&2 echo "Redis is unavailable - waiting"
   sleep 1
 done
